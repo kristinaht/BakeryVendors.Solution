d2a6d655d21de25e0e7a87a8e60da0d3de4caca1
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-13 09:20:10 -0700

    add GetAll and Find methods to vendor class

--- BakeryVendors/Models/Vendor.cs
+++ BakeryVendors/Models/Vendor.cs
@@ -7,16 +7,26 @@ namespace BakeryVendors.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public int Id { get; }
-    private static List<Vendor> _instances = new List<Vendor>{};
+    private static List<Vendor> _vendorInstances = new List<Vendor>{};
     public List<Order> Orders { get; set; }
 
     publid Vendor(string name, string description)
     {
       Name = name;
       Description = description;
-      _instances.Add(this);
-      Id = _instances.Count;
+      _vendorInstances.Add(this);
+      Id = _vendorInstances.Count;
       Orders = new List<Order>(); //add {}?
+    }
+
+    public static List<Vendor> GetAll()
+    {
+      return _vendorInstances;
+    }
+
+    public static Vendor Find(int searchId)
+    {
+      return _vendorInstances[searchId - 1];
     }
   }
 }

6116a3e5e56362347bc624fb63d9ee2354804436
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-13 09:04:50 -0700

    create Order class with properties and private field

---
+++ BakeryVendors/Models/Order.cs
@@ -0,0 +1,11 @@
+namespace BakeryVendors
+{
+  public class Order
+  {
+    string Title { get; set; }
+    string Description { get; set; }
+    string Price { get; set; }
+    string Date { get; set; }
+    // private static List<Order> _orderInstances = new List<Order>{};
+  }
+}

355ed58b425a6b528eeaaf47baf1991c41f0c5d1
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-03-13 09:00:05 -0700

    create vendor constructor

--- BakeryVendors/Models/Vendor.cs
+++ BakeryVendors/Models/Vendor.cs
@@ -5,7 +5,16 @@ namespace BakeryVendors.Models
     public string Name { get; set; }
     public string Description { get; set; }
     public int Id { get; }
-    private static List<Artist> _instances = new List<Artist>{};
+    private static List<Vendor> _instances = new List<Vendor>{};
     public List<Order> Orders { get; set; }
+
+    publid Vendor(string name, string description)
+    {
+      Name = name;
+      Description = description;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>(); //add {}?
+    }
   }
 }
